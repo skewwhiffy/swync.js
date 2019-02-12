00ff0c3d0abc11f745db4f5d6f9f2c2513a70295
Kenny Hung <kenny.hung@oliverwyman.com>
2019-02-12 09:55:44 +0800

    Some TODOs listing paths to implement

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ app.use(express.static('backend/resources/www'));
 app.get('/api', (req, res) => res.send('Hello world'));
 app.get('*', (req, res) => res.sendfile('backend/resources/www/index.html'));
 
+// TODO:
+// files
+// me
+// items
+// music
+
 const port = 38080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
