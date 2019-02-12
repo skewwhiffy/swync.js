0d1a53b6b9c3f62c22d276637da37b9f53c842ea
Kenny Hung <kenny.hung@oliverwyman.com>
2019-02-12 09:04:27 +0800

    Serve vue.js from express application

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world'));
+app.use(express.static('backend/resources/www'));
+app.get('/api', (req, res) => res.send('Hello world'));
+app.get('*', (req, res) => res.sendfile('backend/resources/www/index.html'));
 const port = 38080;
 app.listen(port, () => console.log(`Listening on port ${port}`));
